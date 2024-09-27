71ebc64ee86df393f8ec422879f3174787019fa0
Djthap <Dhananjaythap111@gmail.com>
2024-09-26 20:35:51 -0400

    update

--- db.js
+++ db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://admin:12345@employee.2vr8j0e.mongodb.net/', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb+srv://admin:12345@employee.2vr8j0e.mongodb.net/?retryWrites=true&w=majority&appName=Employee',{});
     console.log('MongoDB Connected');
   } catch (error) {
     console.error(error.message);
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const connectDB = require('./db');
 const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 const extraPriceRoutes = require('./routes/extrapriceRoutes');
-const orderRoutes = require('./routes/orderRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
 const contactUsRoutes = require('./routes/contactUsRoutes');
 
 const app = express();
@@ -16,28 +15,12 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-app.use(
-    cors({
-        origin: 'https://ecommerce-frontend-amber-two.vercel.app',
-        optionsSuccessStatus: 200,
-        allowedOrigins: ['https://ecommerce-frontend-amber-two.vercel.app',"https://ecommerce-frontend-amber-two.vercel.app/"],
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-        allowedHeaders: [
-            'Content-Type',
-            'Authorization',
-            'Access-Control-Allow-Origin',
-            'Access-Control-Allow-Methods',
-            'Access-Control-Allow-Headers',
-        ],
-        credentials: true,
-    })
-)
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/menuItem', menuItemRoutes);
 app.use('/api/extraPrice', extraPriceRoutes);
-app.use('/api/orderRoutes', orderRoutes);
+// app.use('/api/orderRoutes', orderRoutes);
 app.use('/api/contactUsRoutes', contactUsRoutes);
 
 const PORT = process.env.PORT || 5002;
